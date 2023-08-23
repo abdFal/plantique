e3b14f822910b7a5d065d7e81424e6cc41da7429
abdfal <naufaldec07@gmail.com>
2023-08-23 21:34:18 +0700

    add projects

--- script.js
+++ script.js
@@ -12,15 +12,15 @@ function startTime() {
 function checkTime(i) {
   if (i < 10) {
     i = "0" + i;
-  } // add zero in front of numbers < 10
+  }
   return i;
 }
 
 var image = document.getElementsByClassName("image-main");
 new simpleParallax(image, {
-  orientation: "right",
-  transition: "cubic-bezier(0.1,0,0,0.3)",
-  delay: 0.6,
-  scale: 1.7,
+  orientation: "left",
+  transition: "cubic-bezier(0,0,0,0.5)",
+  delay: 0.7,
+  scale: 1.9,
   customWrapper: ".thumbnail",
 });
