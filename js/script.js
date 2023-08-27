240987507f7abddd17ec3040f9e9e781651fb96b
rokan <rokansubhi.f@gmail.com>
2023-08-27 10:52:15 +0700

    rkn

--- js/script.js
+++ js/script.js
@@ -33,5 +33,28 @@ var instance = new simpleParallax(image, {
   customWrapper: ".thumbnail",
 });
 
+var crsr = document.querySelector(".cursor")
+document.addEventListener("mousemove",function(dets){
+    crsr.style.left = dets.x + -5+"px"
+    crsr.style.top = dets.y + -6+"px"
+})
+var boxes = document.querySelectorAll(".box")
+boxes.forEach(function(elem){
+    elem.addEventListener("mouseenter",function(){
+        var att = elem.getAttribute("data-image")
+        crsr.style.width = "470px"
+        crsr.style.height = "370px"
+        crsr.style.borderRadius = "0"
+        crsr.style.backgroundImage = `url(${att})`
+    })
+    elem.addEventListener("mouseleave",function(){
+        elem.style.backgroundColor = "transparent"
+        crsr.style.width = "10px"
+        crsr.style.height = "10px"
+        crsr.style.borderRadius = "50%"
+        crsr.style.backgroundImage = `none`
+    })
+})
+
 // var paper = document.getElementsByClassName("image-paper");
 // new simpleParallax(paper);
